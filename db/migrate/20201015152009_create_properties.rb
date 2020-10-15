4ebb6a042c03f2ef573c7b7da7d41b300570b108
naw linlin min <nodmentor1ygn@gmail.com>
2020-10-15 22:45:44 +0630

    create registration form

---
+++ db/migrate/20201015152009_create_properties.rb
@@ -0,0 +1,12 @@
+class CreateProperties < ActiveRecord::Migration[5.2]
+  def change
+    create_table :properties do |t|
+      t.string :property_name, null: false
+      t.integer :rent, null: false
+      t.text :street_address, null: false
+      t.integer :age, null: false
+      t.text :remark
+      t.timestamps
+    end
+  end
+end
